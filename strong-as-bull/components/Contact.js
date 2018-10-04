2ff03d2c03c1737b8a826268a87722a72ed4ba04
Mathias Wahl <ma10asw@gmail.com>
2018-10-04 13:51:42 +0200

    created dummy methods

---
+++ strong-as-bull/components/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {ScrollView, StyleSheet, Text, View} from "react-native";
+
+
+
+export default class Contact extends React.Component {
+
+    //Prop er unchangable, State endres
+
+    render() {
+        return (
+            <View>
+                <Text> My name is {this.props.name} </Text>
+                <Text> And my children are {this.props.children} </Text>
+                <Text> {this.props.onClickAddContact("This is clicked asdf")} </Text>
+            </View>
+        )
+    }
+
+}
--- strong-as-bull/screens/ContactsScreen.js
+++ strong-as-bull/screens/ContactsScreen.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import {ScrollView, StyleSheet, Text} from "react-native";
+import Contact from '../components/Contact.js';
 
 export default class ContactsScreen extends React.Component {
+
     static navigationOptions = {
         title: 'Contacts',
     };
+
+    constructor(props) {
+        super(props);
+        this.onClickAddContact = this.onClickAddContact.bind(this);
+    }
+
+    onClickAddContact(asdf) {
+        return asdf;
+    }
 
     render() {
         return (
@@ -12,6 +23,10 @@ export default class ContactsScreen extends React.Component {
                 <Text>
                     This is the Contacts screen
                 </Text>
+                <Contact name={"Bob Bob"} onClickAddContact={this.onClickAddContact}>
+                    contact1
+                    contact2
+                </Contact>
             </ScrollView>
         );
     }
