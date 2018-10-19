6358d6598da8b5d7dc0c070bf7d6404ba663b2e1
Sondre Skjåstad <sondre.grav@gmail.com>
2018-10-19 12:39:51 +0200

    Added AsyncStorage functionality

--- strong-as-bull/screens/Goals/GoalsScreen.js
+++ strong-as-bull/screens/Goals/GoalsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View, AsyncStorage } from "react-native";
 import Goal from '../../components/Goal.js';
 
 
@@ -17,16 +17,58 @@ export default class GoalsScreen extends React.Component {
 
     componentDidMount() {
         this.setState({
-            goals: [['Goal 1', 'Description 1'],
-                ['Goal 2', 'Description 2'],
-                ['Goal 3', 'Description 3'],
-                ['Goal 4', 'Description 4'],
-                ['Goal 5', 'Description 5'],
-                ['Goal 6', 'Description 6'],
-                ['Goal 7', 'Description 7'],
-                ['Goal 8', 'Description 8'],]
-        })
+            goals: [
+                    {title: 'Goal 1', description: 'Description 1'},
+                    {title: 'Goal 211', description: 'Description 2'},
+                    {title: 'Goal 3', description: 'Description 3'},
+                    {title: 'Goal 4', description: 'Description 4'},
+                    {title: 'Goal 5', description: 'Description 5'},
+                    {title: 'Goal 6', description: 'Description 6'}
+            ]
+
+
+            //goals: [['Goal 1', 'Description 1'],
+            //    ['Goal 2', 'Description 2'],
+            //    ['Goal 3', 'Description 3'],
+            //    ['Goal 4', 'Description 4'],
+            //    ['Goal 5', 'Description 5'],
+            //    ['Goal 6', 'Description 6'],
+            //    ['Goal 7', 'Description 7'],
+            //    ['Goal 8', 'Description 8'],]
+        });
+        this.storeItem('goals', this.state.goals);
+        const newGoals = this.retrieveItem('goals');
+        this.setState = {
+            goals: newGoals
+        }
     };
+
+    async storeItem(key, item) {
+        try {
+            //we want to wait for the Promise returned by AsyncStorage.setItem()
+            //to be resolved to the actual value before returning the value
+            var jsonOfItem = await AsyncStorage.setItem(key, JSON.stringify(item));
+            return jsonOfItem;
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    async retrieveItem(key) {
+        try {
+            const retrievedItem =  await AsyncStorage.getItem(key);
+
+            let item = JSON.parse(retrievedItem);
+            let goals = [];
+            for (var i=0; JSON.parse(retrievedItem).length; i++){
+                goals.push(JSON.parse(retrievedItem)[i]);
+            }
+            return goals;
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    }
 
     deleteGoal(i) {
         let arr = this.state.goals;
@@ -34,11 +76,17 @@ export default class GoalsScreen extends React.Component {
         this.setState({goals: arr})
     }
 
+    //eachGoal = (goal, i) => {
+    //    return (<Goal key={i} index={i} title={goal[0]} description={goal[1]} deleteGoal={this.deleteGoal.bind(this)}/>)
+    //};
+
     eachGoal = (goal, i) => {
-        return (<Goal key={i} index={i} title={goal[0]} description={goal[1]} deleteGoal={this.deleteGoal.bind(this)}/>)
+        return (<Goal key={i} index={i} title={goal.title} description={goal.description} deleteGoal={this.deleteGoal.bind(this)}/>)
     };
 
     render() {
+
+
         return (
             <View style={styles.container}>
                 <ScrollView style={styles.goalScrollView}>
@@ -46,6 +94,9 @@ export default class GoalsScreen extends React.Component {
                         this.state.goals.map(this.eachGoal)
                     }
                 </ScrollView>
+                <Text>{JSON.stringify(this.state.goals)}</Text>
+                <Text>{goals}</Text>
+
                 <TouchableOpacity style={styles.addButton} onPress={() => this.props.navigation.navigate('CreateGoal')} >
                     <Text style={styles.addButtonText}>+</Text>
                 </TouchableOpacity>
